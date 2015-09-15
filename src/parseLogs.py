5e10c871b24fb6e2ebdf9537fc861747bdf91b44
Stephan Wypler <s.wypler@online.de>
2015-09-15 17:40:46 +0200

    log parser also reads activation threshold

--- src/parseLogs.py
+++ src/parseLogs.py
@@ -8,16 +8,22 @@ if len(sys.argv) < 2:
     sys.exit()
 logfile = sys.argv[1]
 
-activationRegex = re.compile(r'(\d+-\d+-\d+\s\d+:\d+:\d+,\d+):\s+activation\s+of\s+([a-zA-Z]+)\s+after\s+this\s+step:\s+(\d+\.\d+)')
+activationRegex = re.compile(r'(\d+-\d+-\d+\s+\d+:\d+:\d+,\d+):\s+activation\s+of\s+([a-zA-Z]+)\s+after\s+this\s+step:\s+(\d+\.\d+)')
 stepRegex = re.compile(r'###################################### STEP (\d+) ######################################')
+thresholdRegex = re.compile(r'(\d+-\d+-\d+\s+\d+:\d+:\d+,\d+):\s+current\s+activation\s+threshold:\s+(\d+\.\d+)')
 
 behaviours = {
-    "startBehaviour" : None,
-    "goHomeBehaviour" : None,
-    "landBehaviour" : None,
-    "moveBehaviour" : None,
-    "collisionAvoidence" : None,
-    "movementArbiter" : None
+    "PickUpSprayer" : None,
+    "PickUpSander" : None,
+    "PickUpBoard" : None,
+    "PutDownSprayer" : None,
+    "PutDownSander" : None,
+    "PutDownBoard" : None,
+    "SandBoardInHand" : None,
+    "SandBoardInVise" : None,
+    "SprayPaintSelf" : None,
+    "PlaceBoardInVise" : None,
+    "threshold" : None
 }
 
 with open("activationFlow.dat", 'w') as outfile:
@@ -28,16 +34,25 @@ with open("activationFlow.dat", 'w') as outfile:
         for line in infile.readlines():
             match = activationRegex.search(line)
             if match:
+                print match.group(0)
                 dt = datetime.datetime.strptime(match.group(1), "%Y-%m-%d %H:%M:%S,%f")
                 ts = (dt - datetime.datetime(1970, 1, 1)).total_seconds()
                 behaviours[match.group(2)] = (ts, float(match.group(3)))
                 continue
+            match = thresholdRegex.search(line)
+            if match:
+                print match.group(0)
+                dt = datetime.datetime.strptime(match.group(1), "%Y-%m-%d %H:%M:%S,%f")
+                ts = (dt - datetime.datetime(1970, 1, 1)).total_seconds()
+                behaviours["threshold"] = (ts, float(match.group(2)))
+                continue
             match = stepRegex.search(line)
             if match:
+                print match.group(0)
                 try:
                     for (behaviour, data) in behaviours.iteritems():
                         outfile.write(match.group(1) + "\t" + str(data[1]) + "\t")
                     outfile.write("\n")
                 except:
                     pass
-            # "outfile.print("Time	goHomeBehaviour	Time	landBehaviour	Time	MoveBehaviour	Time	selectTargetBehaviour	Time	startBehaviour	Time	activationThreshold\n")
+
