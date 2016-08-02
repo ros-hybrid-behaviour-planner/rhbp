417823fde65c977f1b2ca8ba4bfaf5a03f3d1ce1
Christopher Hrabia <c.hrabia@gmail.com>
2016-08-02 16:19:48 +0200

    no default prefix for planner

--- rhbp_core/src/planner_node.py
+++ rhbp_core/src/planner_node.py
@@ -9,7 +9,7 @@ from behaviour_components.managers import Manager
 
 if __name__ == '__main__':
     rospy.init_node('behaviourPlannerManager', log_level = rospy.DEBUG)
-    m = Manager(activationThreshold = rospy.get_param("~activationThreshold", 7.0), prefix = rospy.get_param("~prefix", "sim")) 
+    m = Manager(activationThreshold = rospy.get_param("~activationThreshold", 7.0), prefix = rospy.get_param("~prefix", ""))
     rate = rospy.Rate(rospy.get_param("~frequency", 1))
     while(not rospy.is_shutdown()):
         m.step()
