7b4a46427c89e40a6abc3a69b836882d734ff483
Phillip Rieger <info@phillip-rieger.de>
2016-12-10 20:40:57 +0100

    Add comments

--- knowledge_base/src/knowledge_base/knowledge_base_manager.py
+++ knowledge_base/src/knowledge_base/knowledge_base_manager.py
@@ -13,6 +13,9 @@ from knowledge_base.msg import Push
 
 
 class KnowledgeBase(object):
+"""
+Wrapper class for accessing the real tupple space
+"""
 
     def __init__(self, name):
         self.__tuple_space = TupleSpace()
@@ -22,11 +25,19 @@ class KnowledgeBase(object):
         self.__pop_service = rospy.Service(name + '/Pop', Pop, self.__pop)
 
     def __del__(self):
+        """
+            closes all services
+        """
         self.__exists_service.shutdown()
         self.__peek_service.shutdown()
         self.__pop_service.shutdown()
 
     def __converts_request_to_tuple_space_format(self, pattern):
+        """
+        :param pattern:  tupple of non-None strings
+        :return: tupple. At each position in the source, where the placeholder * was, is now the typ str
+        """
+        #TODO: use * for placeholder detection
         lst = list(pattern)
         for i in range(0, len(lst)):
             if (lst[i] == None):
@@ -34,12 +45,20 @@ class KnowledgeBase(object):
         return tuple(lst)
 
     def __push(self, request):
+        """
+        Adds the fact to the tupple space
+        :param request: Push message, as defined as ROS message
+        """
         # Since all read request converts nones to string type, it must be done also here.
         # Otherwise the stored tupple can't readed or removed anymore
         converted = self.__converts_request_to_tuple_space_format(request.content)
         self.__tuple_space.add(converted)
 
     def __exists(self, request):
+        """
+        :param request: Exists, as defined as ROS service
+        :return: bool, which indiciated, whether a tupple exists in knowledge base, which matchs the pattern
+        """
         try:
             converted = self.__converts_request_to_tuple_space_format(request.pattern)
             self.__tuple_space.get(converted)
@@ -48,6 +67,11 @@ class KnowledgeBase(object):
             return False
 
     def __peek(self, request):
+        """
+        :param request: Peek, as defined as ROS service
+        :return: PeekResponse, as defined as ROS service respone
+                if a tupple exists in tupple space, an example is returned and the exists flag is setted
+        """
         try:
             converted = self.__converts_request_to_tuple_space_format(request.pattern)
             result = self.__tuple_space.get(converted)
@@ -56,6 +80,13 @@ class KnowledgeBase(object):
             return PeekResponse(exists=False)
 
     def __pop(self, request):
+        """
+        If tupple exists in the tupple space, which matchs the pattern, it will be also removed from  the tupple space
+        and returned
+        :param request: Pop, as defined as ROS service
+        :return: PopResponse, as defined as ROS service respone
+                if a tupple exists in tupple space, an example is returned and the exists flag is setted
+        """
         try:
             converted = self.__converts_request_to_tuple_space_format(request.pattern)
             result = self.__tuple_space.get(converted, remove=True)
