5e7bf655181febfd4368eeba6cbd49a762404471
Phillip Rieger <info@phillip-rieger.de>
2016-12-21 23:11:34 +0100

    also test with irrelevant patterns

--- knowledge_base/src/tests/inverted_tuple_space_test.py
+++ knowledge_base/src/tests/inverted_tuple_space_test.py
@@ -10,16 +10,43 @@ PKG = 'knowledge_base'
 
 class InvertedTupleSpaceTestSuite(unittest.TestCase):
 
-    def test_simple(self):
-        tuple_space = InvertedTupleSpace()
+    @staticmethod
+    def add_not_relevant_patterns(inverted_space):
+        inverted_space.add(('AnyPattern',"Which","Should","Not","Found",str))
+        inverted_space.add(('pos','1',str))
+
+    def run_simple_test(self,inverted_space):
+        """
+        Tests adding and finding a pattern
+        """
         tup = ('pos', '0', '0')
-        tuple_space.add(tup)
-        result = tuple_space.find_for_fact(tup)
+        inverted_space.add(tup)
+        result = inverted_space.find_for_fact(tup)
         self.assertEqual(1, len(result))
         self.assertEqual(tup, result[0])
 
+    def test_simple(self):
+        """
+        Tests adding and finding a pattern, without other patterns
+        """
+        self.run_simple_test(InvertedTupleSpace())
+
+    def test_simple_with_irrelevant_patterns(self):
+        """
+        Tests adding and finding a pattern, with other patterns
+        """
+        inverted_tuple_Space = InvertedTupleSpace()
+        InvertedTupleSpaceTestSuite.add_not_relevant_patterns(inverted_tuple_Space)
+        self.run_simple_test(inverted_tuple_Space)
+
     def test_simple_pattern(self):
+        """
+        Tests the space for a pattern with placeholders
+        """
         tuple_space = InvertedTupleSpace()
+
+        InvertedTupleSpaceTestSuite.add_not_relevant_patterns(tuple_space)
+
         pattern = ('pos', str, str)
         tuple_space.add(pattern)
         result = tuple_space.find_for_fact(('pos', '0', '0'))
@@ -27,11 +54,20 @@ class InvertedTupleSpaceTestSuite(unittest.TestCase):
         self.assertEqual(pattern, result[0])
 
     def test_multiple_patterns(self):
+        """
+        Tests if multiple patterns should found
+        :return:
+        """
         tuple_space = InvertedTupleSpace()
+
+        InvertedTupleSpaceTestSuite.add_not_relevant_patterns(tuple_space)
+        
         pattern1 = ('pos', str, str)
         tuple_space.add(pattern1)
         pattern2 = ('pos', '0', str)
         tuple_space.add(pattern2)
+
+
         result = tuple_space.find_for_fact(('pos', '0', '0'))
         self.assertEqual(2, len(result))
         self.assertTrue(pattern1 in result)
