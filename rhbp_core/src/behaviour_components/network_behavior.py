a4556775f4257716523398ca7f22ed3954c16604
Phillip Rieger <info@phillip-rieger.de>
2017-01-08 17:17:18 +0100

    Finish first version of network_behavior

--- rhbp_core/src/behaviour_components/network_behavior.py
+++ rhbp_core/src/behaviour_components/network_behavior.py
@@ -4,13 +4,16 @@ Created on 13.04.2015
 @author: rieger
 '''
 
-from .activators import BooleanActivator, Condition
+from .activators import Activator, BooleanActivator, Condition
 from .behaviours import BehaviourBase
 from .goals import OfflineGoal
 from .managers import Manager
 
 
 class NetworkBehavior(BehaviourBase):
+
+    MANAGER_POSTFIX = "/Manager"
+
     '''
     Behavior, which contains other behaviors
     Must be in separate file, because of circular dependencies (depends on manager, but manager depends on Behavior)
@@ -19,7 +22,7 @@ class NetworkBehavior(BehaviourBase):
     def __init__(self, effects, name, requires_execution_steps=False, **kwargs):
         '''
 
-        :param effects: tupple <sensor,Effect>
+        :param effects: tuple <sensor,Effect>
         :param name:
         :param requires_execution_steps:
         :param kwargs:
@@ -29,7 +32,7 @@ class NetworkBehavior(BehaviourBase):
         self.requires_execution_steps = requires_execution_steps
         managerArgs = {}
         managerArgs.update(kwargs)
-        managerArgs['prefix'] = name + "/Manager"
+        managerArgs['prefix'] = name + NetworkBehavior.MANAGER_POSTFIX
         self.__manager = Manager(activated=False, **managerArgs)
 
         self.__goal_name_prefix = name + "/Goals/"
@@ -39,20 +42,27 @@ class NetworkBehavior(BehaviourBase):
         for effect in effects:
             correlations.append(effect[1])
             goal_name = self.__generate_goal_name(effect[1])
-            goal = self._create_goal(sensor=effect[0], effect=effect[1], goal_name=goal_name)
-            self.__manager.add_goal()
+            activator_name = self._restore_condition_name_from_pddl_function_name(effect[1].sensorName, effect[0].name)
+            goal = self._create_goal(sensor=effect[0], effect=effect[1], goal_name=goal_name,
+                                     activator_name=activator_name)
+            self.__manager.add_goal(goal)
+        self.correlations=correlations
+
+    def _restore_condition_name_from_pddl_function_name(self, pddl_function_name, sensor_name):
+        return Activator.restore_condition_name_from_pddl_function_name(pddl_function_name=pddl_function_name,
+                                                                        sensor_name=sensor_name)
 
     def __generate_goal_name(self, effect):
         '''
         :param effect: instance of type  Effect
         :return: unique name for goal
         '''
-        # x as seperator between counter an sensor names, to prevent conflict at 'clever' names
+        # x as seperator between counter an sensor names, to prevent conflict not suitable names
         name = self.__goal_name_prefix + str(self.__goal_counter) + 'X' + effect.sensorName
         self.__goal_counter += 1
         return name
 
-    def _create_goal(self, sensor, effect, goal_name):
+    def _create_goal(self, sensor, effect, goal_name, activator_name):
         '''
         :param sensor: instance of type Sensor
         :param effect: instance of type  Effect
@@ -61,9 +71,8 @@ class NetworkBehavior(BehaviourBase):
         '''
         if (effect.sensorType == bool):
             desired_value = True if effect.indicator > 0 else False
-            activator = BooleanActivator(desired_value=desired_value)
-            # TODO set condition effect name
-            condition = Condition(activator=activator)
+            activator = BooleanActivator(name=activator_name, desiredValue=desired_value)
+            condition = Condition(activator=activator, sensor=sensor)
             return OfflineGoal(goal_name, permanent=True, conditions={condition})
 
     def do_step(self):
