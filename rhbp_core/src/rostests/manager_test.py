257d0f82fe232eb07fe489f54448a4163c1f1765
Christopher Hrabia <c.hrabia@gmail.com>
2017-04-03 14:47:14 +0200

    Added test for interruptable property of behaviours

--- rhbp_core/src/rostests/manager_test.py
+++ rhbp_core/src/rostests/manager_test.py
@@ -11,10 +11,10 @@ import unittest
 
 import rospy
 import rostest
-from behaviour_components.activators import Condition, ThresholdActivator, BooleanActivator
+from behaviour_components.activators import Condition, ThresholdActivator, BooleanActivator, GreedyActivator
 from behaviour_components.goals import  GoalBase
 from behaviour_components.managers import Manager
-from behaviour_components.sensors import SimpleTopicSensor
+from behaviour_components.sensors import SimpleTopicSensor, Sensor
 from std_msgs.msg import Bool, Int32
 
 from tests.common import SetTrueBehavior,IncreaserBehavior
@@ -34,6 +34,9 @@ class TestManager(unittest.TestCase):
         rospy.init_node('manager_test_node', log_level=rospy.DEBUG)
 
     def test_independent_behaviour(self):
+        """
+        Test behaviour property independentFromPlanner
+        """
 
         method_prefix = self.__message_prefix + "test_independent_behaviour"
         planner_prefix = method_prefix + "Manager"
@@ -74,36 +77,63 @@ class TestManager(unittest.TestCase):
         self.assertFalse(independent_behaviour3.was_executed, "independent_behaviour2 was executed")
 
     def test_interruptable_behaviour(self):
-
-        #TODO
+        """
+        Test behaviour interruptable property
+        """
 
         method_prefix = self.__message_prefix + "test_interruptable_behaviour"
         planner_prefix = method_prefix + "Manager"
         m = Manager(activationThreshold=7, prefix=planner_prefix)
 
         topic_name_1 = method_prefix + '/sensor_1'
-
-        sensor = SimpleTopicSensor(topic=topic_name_1, message_type=Int32, initial_value=0)
-        condition = Condition(sensor, ThresholdActivator(thresholdValue=3))
-
-        condition_function_name = condition.getFunctionNames()[0]
-        independent_behaviour = IncreaserBehavior(effect_name=condition_function_name, topic_name=topic_name_1,
+        non_interruptable_sensor = SimpleTopicSensor(topic=topic_name_1, message_type=Int32, initial_value=False)
+        non_interruptable_condition = Condition(non_interruptable_sensor, GreedyActivator())
+        condition_function_name = non_interruptable_condition.getFunctionNames()[0]
+        non_interruptable_behaviour = IncreaserBehavior(effect_name=condition_function_name, topic_name=topic_name_1,
                         name=method_prefix + "TopicIncreaser", plannerPrefix=planner_prefix, interruptable=False)
 
-        #TODO add precondition to force stop
+        topic_name_2 = method_prefix + '/sensor_2'
+        interruptable_sensor = SimpleTopicSensor(topic=topic_name_2, message_type=Int32, initial_value=False)
+        interruptable_condition = Condition(interruptable_sensor, GreedyActivator())
+        condition_function_name2 = interruptable_condition.getFunctionNames()[0]
+        interruptable_behaviour = IncreaserBehavior(effect_name=condition_function_name2, topic_name=topic_name_2,
+                        name=method_prefix + "TopicIncreaser2", plannerPrefix=planner_prefix, interruptable=True)
 
-        goal = GoalBase(method_prefix + 'CentralGoal', plannerPrefix=planner_prefix)
-        goal.addCondition(condition)
+        enable_sensor = Sensor(name='enable_sensor', initial_value=True)
+        enable_cond = Condition(enable_sensor, BooleanActivator())
+        non_interruptable_behaviour.addPrecondition(enable_cond)
 
-        for x in range(0, 5, 1):
+        goal = GoalBase(method_prefix + 'CentralGoal', plannerPrefix=planner_prefix, permanent=True)
+        goal.addCondition(non_interruptable_condition)
+        goal.addCondition(interruptable_condition)
+
+        # first normal operation: every behaviour runs as expected
+        for x in range(0, 4, 1):
             m.step()
             rospy.sleep(0.1)
 
-        goal_proxy = m.goals[0]
-        goal_proxy.sync()
-        self.assertTrue(goal_proxy.satisfied, 'Goal is not satisfied')
+        self.assertTrue(non_interruptable_behaviour._isExecuting, "Non-Interruptable Behaviour is not executed")
+        self.assertTrue(interruptable_behaviour._isExecuting, "Interruptable Behaviour is not executed")
 
-        self.assertTrue(independent_behaviour._isExecuting, "Independent Behaviour is not executed")
+        #disable non_interruptable_behaviour precondition and check if it is affected
+        enable_sensor.update(False)
+        for x in range(0, 1, 1):
+            m.step()
+            rospy.sleep(0.1)
+
+        self.assertTrue(non_interruptable_behaviour._isExecuting, "Non-Interruptable Behaviour is not executed")
+        self.assertTrue(interruptable_behaviour._isExecuting, "Interruptable Behaviour is not executed")
+
+        #disable precondition of interruptable behaviour and check if it is disabled as well
+
+        interruptable_behaviour.addPrecondition(enable_cond)
+
+        for x in range(0, 1, 1):
+            m.step()
+            rospy.sleep(0.1)
+
+        self.assertTrue(non_interruptable_behaviour._isExecuting, "Non-Interruptable Behaviour is not executed")
+        self.assertFalse(interruptable_behaviour._isExecuting, "Interruptable Behaviour is executed")
 
 
 if __name__ == '__main__':
