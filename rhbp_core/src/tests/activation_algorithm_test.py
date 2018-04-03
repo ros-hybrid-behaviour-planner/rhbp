4c800ce926a0f2057b81dc4e19cd438cb25ee301
Christopher Hrabia <c.hrabia@gmail.com>
2018-04-03 16:52:24 +0200

    Made ActivationAlgorithmTestSuit more robust for future extensions

--- rhbp_core/src/tests/activation_algorithm_test.py
+++ rhbp_core/src/tests/activation_algorithm_test.py
@@ -8,17 +8,25 @@ from behaviour_components.activation_algorithm import ActivationAlgorithmFactory
 
 from behaviour_components.managers import Manager
 
+
 class ActivationAlgorithmTestSuite(unittest.TestCase):
+
+    def setUp(self):
+
+        self.manager = Manager()
+
+    def tearDown(self):
+
+        self.manager.unregister()
 
     def test_factory(self):
 
-        manager = Manager()
-
-        algo = ActivationAlgorithmFactory.create_algorithm("default", manager)
+        algo = ActivationAlgorithmFactory.create_algorithm("default", self.manager)
 
         self.assertNotEqual(algo, None, 'Algorithm is none')
 
         self.assertTrue(isinstance(algo, BaseActivationAlgorithm), "Not the correct algorithm")
+
 
 if __name__ == '__main__':
     unittest.main()
