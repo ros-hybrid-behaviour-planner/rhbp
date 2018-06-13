098c48111aca035d5b0b5d667189323ffa5c607b
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-06-13 13:49:35 +0200

    Goal timeout handling

--- rhbp_core/src/behaviour_components/goals.py
+++ rhbp_core/src/behaviour_components/goals.py
@@ -292,6 +292,7 @@ class GoalProxy(AbstractGoalRepresentation):
     '''
 
     SERVICE_TIMEOUT = 2
+    MAX_CONSECUTIVE_TIMEOUTS = 10
 
     def __init__(self, name, permanent, planner_prefix):
         '''
@@ -300,6 +301,7 @@ class GoalProxy(AbstractGoalRepresentation):
         super(GoalProxy, self).__init__(name, permanent)
         self._service_prefix = planner_prefix + '/' + self._name + '/'
         self.__old_PDDL = (PDDL(statement=name), PDDL(statement="", predicates=[], functions=[]))
+        self.__consecutive_timeouts = 0
 
     def fetchPDDL(self):
         '''
@@ -308,6 +310,7 @@ class GoalProxy(AbstractGoalRepresentation):
         try:
             rhbplog.logdebug("Waiting for service %s", self._service_prefix + 'PDDL')
             rospy.wait_for_service(self._service_prefix + 'PDDL', timeout=self.SERVICE_TIMEOUT)
+            self.__consecutive_timeouts = 0
         except rospy.ROSException:
             self._handle_service_timeout()
             return self.__old_PDDL
@@ -332,6 +335,7 @@ class GoalProxy(AbstractGoalRepresentation):
         try:
             rhbplog.logdebug("Waiting for service %s", self._service_prefix + 'GetStatus')
             rospy.wait_for_service(self._service_prefix + 'GetStatus', timeout=self.SERVICE_TIMEOUT)
+            self.__consecutive_timeouts = 0
         except rospy.ROSException:
             self._handle_service_timeout()
             # just return (old values will still be used)
@@ -361,26 +365,31 @@ class GoalProxy(AbstractGoalRepresentation):
         """
         basically deactivate the goal in case a service has timeout
         """
-        rhbplog.logerr("ROS service timeout of goal '%s': %s. Fulfillment will be reset", self._name,
+        rhbplog.logerr("ROS service timeout of goal '%s': %s.", self._name,
                      traceback.format_exc())
-        # self._active = False
-        # self.fulfillment = 0.0
-        # TODO make sure this doesnt go on forever
+
+        self.__consecutive_timeouts += 1
+
+        if self.__consecutive_timeouts > self.MAX_CONSECUTIVE_TIMEOUTS:
+            rhbplog.logerr("Too many consecutive timeouts for goal '%s'! Fulfillment will be reset", self._name)
+            self._active = False
+            self.fulfillment = 0.0
 
     @AbstractGoalRepresentation.activated.setter
     def activated(self, value):
-        self._activated = value
-        #inform remote goal about new activated state
+        # inform remote goal about new activated state
         service_name = self._service_prefix + 'Activate'
         rhbplog.logdebug("Waiting for service %s", service_name)
         try:
             rospy.wait_for_service(service_name, timeout=self.SERVICE_TIMEOUT)
+            self.__consecutive_timeouts = 0
         except rospy.ROSException:
-            # TODO this is a setter -> needs to be handled differently
-            pass
+            self._handle_service_timeout()
+            return
         try:
             activateRequest = rospy.ServiceProxy(service_name, Activate)
             activateRequest(value)
+            self._activated = value
         except rospy.ServiceException:
             rhbplog.logerr("ROS service exception in 'activated' of goal '%s': %s", self._name,
                          traceback.format_exc())
