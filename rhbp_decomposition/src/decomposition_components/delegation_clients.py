b9c70f6be8907fbdc33f687628cf1c74275aba85
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-07-11 17:00:43 +0200

    added client for delegable behaviour

--- rhbp_decomposition/src/decomposition_components/delegation_clients.py
+++ rhbp_decomposition/src/decomposition_components/delegation_clients.py
@@ -35,23 +35,121 @@ class RHBPDelegationClient(DelegationClientBase):
         if not self._active_manager:
             raise RuntimeError("Delegation without a registered DelegationManager")
 
-        if conditions is None:
-            self.logger.logwarn("Trying to delegate a goal without conditions")
-            conditions = []
-
-        condition_string = "\n\t".join([str(x) for x in conditions])
-        self.logger.loginfo("New delegation attempt with the conditions:\n\t" + condition_string + "\n\t and the satisfaction threshold of " + str(satisfaction_threshold))
-
-        new_goal_wrapper = RHBPGoalWrapper(name=goal_name, conditions=conditions, satisfaction_threshold=satisfaction_threshold)
+        new_goal_wrapper = self.build_goal_wrapper(conditions=conditions, goal_name=goal_name, satisfaction_threshold=satisfaction_threshold)
 
         delegation_id = self.delegate_goal_wrapper(goal_wrapper=new_goal_wrapper)
         self.logger.loginfo("Delegation has local ID " + str(delegation_id))
 
         return delegation_id
 
+    def build_goal_wrapper(self, conditions, goal_name, satisfaction_threshold=1.0):
+        """
+        Builds a GoalWrapper from given parameters
+
+        :param conditions: list of conditions
+        :type conditions: list
+        :param goal_name: name of the goal
+        :type goal_name: str
+        :param satisfaction_threshold: satisfaction threshold of the goal
+        :type satisfaction_threshold: float
+        :return: the built GoalWrapper
+        :rtype: RHBPGoalWrapper
+        """
+        if conditions is None:
+            self.logger.logwarn("Trying to delegate a goal without conditions")
+            conditions = []
+
+        condition_string = "\n\t".join([str(x) for x in conditions])
+
+        self.logger.loginfo("New delegation attempt with the conditions:\n\t" + condition_string + "\n\t and the satisfaction threshold of " + str(satisfaction_threshold))
+        new_goal_wrapper = RHBPGoalWrapper(name=goal_name, conditions=conditions, satisfaction_threshold=satisfaction_threshold)
+        return new_goal_wrapper
+
     def start_work(self, delegation_id):
         # This should never happen
         self.logger.logerr(msg="Unexpected need to start work for the delegation "+str(delegation_id)+" in the DelegationClient with the ID "+str(self.id))
+
+
+class RHBPClientForDelegable(RHBPDelegationClient):
+    """
+    Version of the RHBPDelegationClient used for units that can do the work
+    themselves if needed (the DelegableBehaviour)
+    """
+
+    def __init__(self):
+        super(RHBPClientForDelegable, self).__init__()
+        self._work_function_dictionary = {}
+
+    def delegate(self, goal_name, conditions=None, satisfaction_threshold=1.0, own_cost=-1, start_work_function=None):
+        """
+        Tries to delegate a goal with given parameters, gives a proposal
+        themselves if given a positive own cost
+
+        :param goal_name: name of the goal
+        :type goal_name: str
+        :param conditions: a list of conditions
+        :type conditions: list
+        :param satisfaction_threshold: the satisfaction threshold of the goal
+        :type satisfaction_threshold: float
+        :param own_cost: Cost to do the task myself, set to -1 if it cant be
+                performed
+        :type own_cost: float
+        :param start_work_function: function pointer on function that needs to
+                be invoked should i need to perform the work myself, setting
+                this is important should the own_cost be set to 0 or higher
+        :type start_work_function: pointer on a function
+        :return: ID of the delegation
+        :rtype: int
+        :raises RuntimeError: if no DelegationManager is registered
+        :raises RuntimeError: if own_cost>=0 and no start_work_function is given
+        """
+
+        if not self._active_manager:
+            raise RuntimeError("Delegation without a registered DelegationManager")
+
+        if own_cost >= 0 and start_work_function is None:
+            raise RuntimeError("Delegation with set own cost but no given work_function")
+
+        new_goal_wrapper = self.build_goal_wrapper(conditions=conditions, goal_name=goal_name, satisfaction_threshold=satisfaction_threshold)
+
+        delegation_id = self.delegate_goal_wrapper(goal_wrapper=new_goal_wrapper, own_cost=own_cost)
+        self.logger.loginfo("Delegation has local ID " + str(delegation_id))
+
+        if own_cost >= 0:
+            self._add_work_function(delegation_id=delegation_id, start_work_function=start_work_function)
+
+        return delegation_id
+
+    def _add_work_function(self, delegation_id, start_work_function):
+        """
+        Adds a start_work_function for the delegation with the given ID
+
+        :param delegation_id: ID of the delegation
+        :type delegation_id: int
+        :param start_work_function: pointer on the start_work_function
+        :type start_work_function: pointer on a function
+        """
+
+        self._work_function_dictionary[delegation_id] = start_work_function
+
+    def start_work(self, delegation_id):
+
+        self.logger.loginfo("Won the auction for the delegation "+str(delegation_id)+" myself, starting to work myself")
+        try:
+            func = self._work_function_dictionary[delegation_id]
+        except KeyError:
+            self.logger.logerr("No start_work_function given for delegation "+str(delegation_id)+": No work will be done here!")
+            raise
+
+        # start the working then return
+        func()
+        return
+
+    def terminate_delegation(self, delegation_id):
+        super(RHBPClientForDelegable, self).terminate_delegation(delegation_id=delegation_id)
+
+        if self._work_function_dictionary.keys().__contains__(delegation_id):
+            del self._work_function_dictionary[delegation_id]
 
 
 class RHBPManagerDelegationClient(RHBPDelegationClient):
--- rhbp_decomposition/src/tests/delegation_client_unit_test.py
+++ rhbp_decomposition/src/tests/delegation_client_unit_test.py
@@ -1,7 +1,7 @@
 
-from decomposition_components.delegation_clients import RHBPDelegationClient, RHBPManagerDelegationClient
+from decomposition_components.delegation_clients import RHBPDelegationClient, RHBPManagerDelegationClient, RHBPClientForDelegable
 from decomposition_components.cost_computing import PDDLCostEvaluator
-from delegation_tests.test_utils import MockedDelegationManager
+from delegation_tests.test_utils import MockedDelegationManager, FunctionPointerTester
 import unittest
 
 
@@ -64,6 +64,59 @@ class RHBPClientsTest(unittest.TestCase):
         self.assertEqual(self.mockedDM.clients[0], uut._client_id)
         self.assertTrue(uut._active_manager)
         self.assertEqual(uut._delegation_manager, self.mockedDM)
+
+    def test_delegable_delegate(self):
+        uut = RHBPClientForDelegable()
+
+        goal_name = "test_goal"
+        conditions = ["test_conditions"]
+        threshold = 0.9
+
+        # base cases
+        self.assertRaises(RuntimeError, uut.delegate, goal_name, conditions, threshold)
+
+        uut.register(delegation_manager=self.mockedDM)
+        delegation_id = uut.delegate(goal_name=goal_name, conditions=conditions, satisfaction_threshold=threshold)
+
+        self.assertEqual(delegation_id, 1)
+        self.assertEqual(self.mockedDM.goal_wrapper._conditions, conditions)
+        self.assertEqual(self.mockedDM.goal_wrapper._satisfaction_threshold, threshold)
+
+        # with own cost
+        uut = RHBPClientForDelegable()
+        uut.register(delegation_manager=self.mockedDM)
+        fpt = FunctionPointerTester()
+        own_cost = 2.3
+
+        self.assertRaises(RuntimeError, uut.delegate, goal_name, conditions, threshold, own_cost)
+
+        delegation_id = uut.delegate(goal_name=goal_name, conditions=conditions, satisfaction_threshold=threshold, own_cost=own_cost, start_work_function=fpt.function)
+
+        self.assertEqual(delegation_id, 1)
+        self.assertEqual(self.mockedDM.goal_wrapper._conditions, conditions)
+        self.assertEqual(self.mockedDM.goal_wrapper._satisfaction_threshold, threshold)
+        self.assertEqual(uut._work_function_dictionary[delegation_id], fpt.function)
+
+    def test_work_function_dict(self):
+        uut = RHBPClientForDelegable()
+        uut.register(delegation_manager=self.mockedDM)
+        fpt = FunctionPointerTester()
+        own_cost = 2.3
+        goal_name = "test_goal"
+        conditions = ["test_conditions"]
+        threshold = 0.9
+
+        self.assertRaises(RuntimeError, uut.delegate, goal_name, conditions, threshold, own_cost)
+
+        delegation_id = uut.delegate(goal_name=goal_name, conditions=conditions, satisfaction_threshold=threshold, own_cost=own_cost, start_work_function=fpt.function)
+
+        uut.start_work(delegation_id=delegation_id)
+        self.assertTrue(fpt.function_called)
+
+        self.assertRaises(KeyError, uut.start_work, delegation_id+1)
+
+        uut.terminate_delegation(delegation_id=delegation_id)
+        self.assertRaises(KeyError, uut.start_work, delegation_id)
 
 
 if __name__ == '__main__':
