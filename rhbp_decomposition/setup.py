e939d6ff6c76b65d690598bcb4e7253c3998eefb
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-07-14 18:19:13 +0200

    style update

--- rhbp_decomposition/setup.py
+++ rhbp_decomposition/setup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python2
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
