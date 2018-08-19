e75dc4d81770cbb28599f30a40d4ab54a10e8114
Vito Mengers <v.mengers@campus.tu-berlin.de>
2018-08-19 16:20:32 +0200

    touchups for the delegation goal

--- rhbp_decomposition/src/decomposition_components/delegation_goal.py
+++ rhbp_decomposition/src/decomposition_components/delegation_goal.py
@@ -2,48 +2,123 @@
 from decomposition_components.delegation_clients import RHBPDelegationClient
 from decomposition_components.goal_wrapper import RHBPGoalWrapper, DecompositionGoal
 from delegation_components.delegation_manager import DelegationManager
+from behaviour_components.goals import rhbplog
 
 
 class RunningGoalWrapper(RHBPGoalWrapper):
+    """
+    Wrapper for a already running RHBP goal (GoalBase), that implements
+    start_with_contractor and terminate_goal
+    Goal should not be registered at construction of wrapper
+    """
 
-    def __init__(self, name, goal, conditions, satisfaction_threshold=1.0):
-        super(RunningGoalWrapper, self).__init__(name=name, conditions=conditions, satisfaction_threshold=satisfaction_threshold)
+    def __init__(self, goal, conditions):
+        """
+        Constructor
+
+        :param goal: unregistered RHBP goal that will be wrapped by this
+        :type goal: GoalBase
+        :param conditions: list of conditions of the goal (should be exactly the
+                same as the conditions of the goal)
+        :type conditions: list
+        """
+
+        super(RunningGoalWrapper, self).__init__(name=goal.name, conditions=conditions,
+                                                 satisfaction_threshold=goal.satisfaction_threshold)
         self._goal = goal
 
     def __del__(self):
+        """
+        Destructor
+        """
+
         del self._conditions
 
     def send_goal(self, name=""):
+        """
+        Transfers the planner prefix of a Manager to this goal, so that it will
+        register there
+
+        :param name: plannerPrefix of a Manager in the system
+        :type name: str
+        """
+
         self._goal.start_with_contractor(planner_prefix=name)
         self._created_goal = True
 
     def terminate_goal(self):
+        """
+        Makes sure the goal unregsiters
+        """
+
         self._created_goal = False
         self._goal.terminate_goal()
 
 
 class DelegationGoal(DecompositionGoal):
+    """
+    Goal for the RHBP that will determine the best suited agent in the system
+    and registers itself there
 
-    def __init__(self, name, delegation_manager, permanent=False, conditions=None, plannerPrefix="", priority=0, satisfaction_threshold=1.0, activated=True):
+    Use start_auction and finish_auction to start/finish auction for best
+    suited agent
+    """
+
+    def __init__(self, name, delegation_manager, permanent=False, conditions=None, priority=0, satisfaction_threshold=1.0, activated=True):
+        """
+        Constructor
+
+        :param name: name of the goal
+        :type name: str
+        :param delegation_manager: DelegationManager used by this goal
+        :type delegation_manager: DelegationManager
+        :param permanent: whether this goal is permanent
+        :type permanent: bool
+        :param conditions: conditions of this goal
+        :type conditions: list
+        :param priority: priority of this goal
+        :type priority: int
+        :param satisfaction_threshold: satisfaction_threshold of this goal
+        :type satisfaction_threshold: float
+        :param activated: whether this goal is activated or not at beginning
+        :type activated: bool
+        """
+
         self._contractor_found = False
         super(DelegationGoal, self).__init__(name=name, permanent=permanent, conditions=conditions,
-                                             plannerPrefix=plannerPrefix, priority=priority,
+                                             plannerPrefix="TBD", priority=priority,
                                              satisfaction_threshold=satisfaction_threshold, activated=activated)
         self._client = RHBPDelegationClient()
         self._client.register(delegation_manager=delegation_manager)
         self._goal_wrapper = None
         self._delegation_id = -1
+        self._auction_running = False
+
+    def __del__(self):
+        """
+        Destructor
+        """
+
+        self._contractor_found = False
+        if self._goal_wrapper is not None:
+            self._goal_wrapper.__del__()
+        self._client.__del__()
+        super(DelegationGoal, self).__del__()
 
     def _init_services(self):
+        """
+        Initializes services, if a contractorhas been found
+        """
 
         if not self._contractor_found:
             return
 
-        print "starting services"
         super(DelegationGoal, self)._init_services()
-        print "done"
 
     def register(self):
+        """
+        Registers at the manager, if the right ine has been determined yet
+        """
 
         if not self._contractor_found:
             return
@@ -51,23 +126,81 @@ class DelegationGoal(DecompositionGoal):
         super(DelegationGoal, self).register()
 
     def start_with_contractor(self, planner_prefix):
+        """
+        Starts services and registers with the given planner
+        
+        :param planner_prefix: prefix of the planner, that will have this goal
+                now
+        :type planner_prefix: str
+        """
+
         self._planner_prefix = planner_prefix
         self._contractor_found = True
         self._init_services()
         self.register()
 
     def terminate_goal(self):
+        """
+        Unregisters this goal, but checks if it is registered before
+        """
+
         if hasattr(self, '_registered') and self._registered:
             self.unregister(terminate_services=True)
 
     def start_auction(self):
-        self._goal_wrapper = RunningGoalWrapper(name=self.name, goal=self,
-                                                conditions=self._conditions,
-                                                satisfaction_threshold=self.satisfaction_threshold)
+        """
+        Starts the process of determining the right agent in the system for
+        this goal
+
+        Wait for some time depending on system specs before finishing auction
+        """
+
+        self._goal_wrapper = RunningGoalWrapper(goal=self,
+                                                conditions=self._conditions)
         self._delegation_id = self._client.delegate_goal_wrapper(goal_wrapper=self._goal_wrapper,
                                                                  known_depth=0)
+        self._auction_running = True
 
     def finish_auction(self):
+        """
+        Finishes the process of determining the right agent in the system for
+        this goal
+
+        :return: Whether the process was successful or not
+        :rtype: bool
+        """
+
+        if not self._auction_running:
+            rhbplog.logwarn("Trying to finish an auction that has not started yet for DelegationGoal"+self.name)
+            return False
+
         for i in range(DelegationManager.DEFAULT_AUCTION_STEPS):
             self._client.do_step()
+
+        if not self._contractor_found:
+            rhbplog.logwarn("Auction was not finished successful (see other logging), you can try again to finish after some additional time")
+
+        return self._contractor_found
+
+    @property
+    def auction_running(self):
+        """
+        Whether or not the auction is running
+
+        :return: whether or not the auction is running
+        :rtype: bool
+        """
+
+        return self._auction_running
+
+    @property
+    def contractor_found(self):
+        """
+        Whether or not a contractor has been found
+
+        :return: whether or not a contractor has been found
+        :rtype: bool
+        """
+
+        return self._contractor_found
 
