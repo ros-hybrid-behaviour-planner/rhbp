2ed624c7051c1f12b2706a00756200a33b568b11
Christopher Hrabia <c.hrabia@gmail.com>
2018-11-09 11:35:27 +0100

    extendend negation tests

--- rhbp_core/src/tests/negation_test.py
+++ rhbp_core/src/tests/negation_test.py
@@ -119,6 +119,52 @@ class NegationTestSuite(unittest.TestCase):
         self.assertAlmostEqual(con_wish.indicator, 0.0, places=3)
         self.assertAlmostEqual(neg_con_wish.indicator, 1.0, places=3)
 
+    def test_negation_boolean_true(self):
+        mock_sensor = sensors.Sensor(initial_value=True)
+
+        activator = activators.BooleanActivator(desiredValue=False)
+
+        condition = conditions.Condition(sensor=mock_sensor, activator=activator)
+
+        neg_condition = conditions.Negation(condition)
+
+        neg_condition.updateComputation()
+
+        con_satisfaction = condition.satisfaction
+        neg_con_satisfaction = neg_condition.satisfaction
+
+        self.assertAlmostEqual(con_satisfaction, 0.0, places=3)
+        self.assertAlmostEqual(neg_con_satisfaction, 1.0, places=3)
+
+        con_wish = condition.getWishes()[0]
+        neg_con_wish = neg_condition.getWishes()[0]
+
+        self.assertAlmostEqual(con_wish.indicator, -1.0, places=3)
+        self.assertAlmostEqual(neg_con_wish.indicator, 0.0, places=3)
+
+    def test_negation_boolean_false(self):
+        mock_sensor = sensors.Sensor(initial_value=False)
+
+        activator = activators.BooleanActivator(desiredValue=False)
+
+        condition = conditions.Condition(sensor=mock_sensor, activator=activator)
+
+        neg_condition = conditions.Negation(condition)
+
+        neg_condition.updateComputation()
+
+        con_satisfaction = condition.satisfaction
+        neg_con_satisfaction = neg_condition.satisfaction
+
+        self.assertAlmostEqual(con_satisfaction, 1.0, places=3)
+        self.assertAlmostEqual(neg_con_satisfaction, 0.0, places=3)
+
+        con_wish = condition.getWishes()[0]
+        neg_con_wish = neg_condition.getWishes()[0]
+
+        self.assertAlmostEqual(con_wish.indicator, 0.0, places=3)
+        self.assertAlmostEqual(neg_con_wish.indicator, 1.0, places=3)
+
 
 if __name__ == '__main__':
     unittest.main()
