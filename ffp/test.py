de7289318de5351e1585612a3dbc82cdf51fafac
Christopher Hrabia <c.hrabia@gmail.com>
2019-02-06 15:45:20 +0100

    improved ffp test example
    Former-commit-id: f4b878c5e1bb59eaace2a9e1ae87a009c62375fb

--- ffp/test.py
+++ ffp/test.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python2
 import ffp
+
 with open("../metric_ff/robotDomain.pddl", "r") as infile:
     domain = "".join(infile.readlines())
 
 with open("../metric_ff/robotProblem.pddl", "r") as infile:
     problem = "".join(infile.readlines())
 
-print ffp.plan(domain, problem)
+
+plan = ffp.plan(domainPDDL=domain,
+                problemPDDL=problem,
+                searchMode=5,
+                upperCostBound=1000,
+                weight=5,
+                costMinimization=True,
+                debug=0)
+
+print("Plan Result:", plan)
 
